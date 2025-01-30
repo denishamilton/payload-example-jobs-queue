ff4b16af8a993c4197ff2e5c8773107c04375309
den hamiltom <hamilton.den@mail.ru>
2025-01-30 22:04:25 +0100

    Audio transcript code

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,12 +3,13 @@ import { sqliteAdapter } from '@payloadcms/db-sqlite'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
-import { buildConfig } from 'payload'
+import { buildConfig, TaskConfig } from 'payload'
 import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import sharp, { queue } from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { title } from 'process'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,7 +21,68 @@ export default buildConfig({
       baseDir: path.resolve(dirname),
     },
   },
-  collections: [Users, Media],
+
+  jobs: {
+    access: {
+      run: () => true,
+    },
+    addParentToTaskLog: true,
+    autoRun: [
+      {
+        queue: 'default',
+        cron: '* * * * *',
+        limit: 2,
+      },
+    ],
+    tasks: [
+      {
+        slug: 'transcribeAudio',
+        retries: 2,
+        inputSchema: [
+          {
+            name: 'audioNodeId',
+            type: 'number',
+            required: true,
+          },
+        ],
+        outputSchema: [],
+        handler: transcribrAudioHandler,
+        onSuccess: async () => {
+          console.log('success')
+        },
+        label: 'Transcribe Audio',
+      },
+    ],
+  },
+
+  collections: [
+    Users,
+    Media,
+
+    {
+      slug: 'audioNotes',
+      fields: [
+        {
+          name: 'transcriptionError',
+          type: 'text',
+        },
+      ],
+      hooks: {
+        afterChange: [
+          async ({ doc, req: { payload } }) => {
+            console.log('Queueing task for doc: ', doc.id)
+            const job = await payload.jobs.queue({
+              task: 'transcribeAudio',
+              input: { audioNoteId: doc.id },
+              queue: 'default',
+            })
+            console.log('Job queued: ', job.id)
+          },
+        ],
+      },
+    },
+  ],
+
   editor: lexicalEditor(),
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
